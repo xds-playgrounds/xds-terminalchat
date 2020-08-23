888c783d3a0e158ffc102c28f978d00393d1d6ab
soma42 <soma42@protonmail.ch>
2020-08-24 00:29:34 +0200

    use NetDriver everywhere

--- src/XDS.TerminalChat/App.cs
+++ src/XDS.TerminalChat/App.cs
@@ -41,6 +41,7 @@ namespace XDS.Messaging.TerminalChat
 
         public static void ShowUI()
         {
+            Application.UseSystemConsole = true;
             Application.Init();
             
             var topLevel = Application.Top;
